6baca1c9b4b7a063b83e3e0f92598465f835a329
StevenL94 <steven_lee_13@hotmail.com>
2016-02-15 22:01:22 -0500

    Initial commit

---
+++ Multi-Layer Perceptron/Multi-Layer Perceptron/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Multi-Layer Perceptron
+//
+//  Created by Steven Lee on 2/15/16.
+//  Copyright © 2016 Steven Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
